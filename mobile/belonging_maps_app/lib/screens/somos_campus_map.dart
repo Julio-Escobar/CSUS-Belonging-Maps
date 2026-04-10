3b35f54d3d41cc3a3dec8f179863c7e466161503
TravMali <travismalizia@gmail.com>
2026-04-09 23:34:09 -0700

    somos_campus_map has functional placeholder layer url.

--- mobile/belonging_maps_app/lib/screens/somos_campus_map.dart
+++ mobile/belonging_maps_app/lib/screens/somos_campus_map.dart
@@ -7,18 +7,27 @@ class SomosCampusMap extends StatelessWidget {
 
 @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //Map with basemap option.
-      body: ArcGISMapView(
+
+    final map = ArcGISMap.withBasemapStyle(BasemapStyle.openStreets)
+      // Set location here. Scale is zoom (bigger number is less zoom).
+      ..initialViewpoint = Viewpoint.withLatLongScale(
+        latitude: 38.56091,   
+        longitude: -121.42405, 
+        scale: 10000,
+      );
+
+    //Replace layerURL with the URL of desired feature layer. Placeholder: Community Centers in Sacramento        
+    final String layerURL = "https://services5.arcgis.com/54falWtcpty3V47Z/arcgis/rest/services/City_of_Sacramento_Community_Centers/FeatureServer/0";
+    final featureTable = ServiceFeatureTable.withUri(Uri.parse(layerURL));
+    final featureLayer = FeatureLayer.withFeatureTable(featureTable);
+
+    map.operationalLayers.add(featureLayer);
+
+      return Scaffold(
+        body: ArcGISMapView(
         controllerProvider: () => ArcGISMapView.createController()
-          ..arcGISMap = (ArcGISMap.withBasemapStyle(BasemapStyle.openStreets)
-            //Set location here. Scale is zoom (bigger number is less zoom).
-            ..initialViewpoint = Viewpoint.withLatLongScale(
-              latitude: 38.56091,   
-              longitude: -121.42405, 
-              scale: 10000,         
-            )),
-      ),
-    );
+          ..arcGISMap = map,
+          ),
+      );
   }
 }
